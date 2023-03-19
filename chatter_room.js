aab18346ad296935d4f809f4a09bc3ca27b15d88
KayaanPro <103744889+KayaanPro@users.noreply.github.com>
2023-03-19 12:29:39 -0400

    Add files via upload

---
+++ chatter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8Pk6a9gQHlpFNUT8h7FtoEgCCdQSjtUM",
+    authDomain: "chatterapp-71497.firebaseapp.com",
+    databaseURL: "https://chatterapp-71497-default-rtdb.firebaseio.com",
+    projectId: "chatterapp-71497",
+    storageBucket: "chatterapp-71497.appspot.com",
+    messagingSenderId: "99743146951",
+    appId: "1:99743146951:web:1d7d85a0426ce3092c6fe2"
+  };
+
+  firebase.initializeApp(firebaseConfig);
